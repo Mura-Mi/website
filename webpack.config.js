297c83a8075bcba8325683a0d0f9f43d8f160242
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-09-07 15:26:25 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: [
+        "./js/main.js"
+    ],
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loaders: [ "style", "css" ] },
+            { test: /\.sass$/, loaders: [ "style", "css", "sass" ]},
+            { test: /\.scss$/, loaders: [ "style", "css", "sass" ]},
+            { test: /\.svg$/, loader: 'url-loader?mimetype=image/svg+xml' },
+            { test: /\.woff$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.woff2$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.eot$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.ttf$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?mimetype=image/svg+xml' },
+            { test: /\.woff(\d+)?(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?mimetype=application/font-woff' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?mimetype=application/font-woff' }
+        ]
+    }
+};
